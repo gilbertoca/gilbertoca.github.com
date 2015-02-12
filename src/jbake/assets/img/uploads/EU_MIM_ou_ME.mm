afc0c7bf77e5d42f2fca51c55bb9480d210091dd
gilbertoca <gilbertoca@gmail.com>
2015-02-12 15:36:10 -0300

    bring images to this branch

---
+++ src/jbake/assets/img/uploads/EU_MIM_ou_ME.mm
@@ -0,0 +1,44 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<attribute_registry SHOW_ATTRIBUTES="selected"/>
+<node COLOR="#000000" CREATED="1384533884973" ID="ID_1913086974" MODIFIED="1384534801304" STYLE="fork" TEXT="EU, MIM ou ME?" VGAP="1">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1384533961517" HGAP="57" ID="ID_336313566" MODIFIED="1384535386530" POSITION="right" TEXT="MIM" VSHIFT="-33">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1384533970275" ID="ID_1043506676" MODIFIED="1384534798774" TEXT="complemento (final de frase)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1384533984437" ID="ID_1742236723" MODIFIED="1384534798774" TEXT="depois de ENTRE">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1384533992747" ID="ID_1334370582" MODIFIED="1384534798774" TEXT="antes de infinitivo (na ordem indireta)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1384534131689" HGAP="55" ID="ID_399400907" MODIFIED="1384535383820" POSITION="left" TEXT="EU" VSHIFT="-11">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1384534136560" ID="ID_992036384" MODIFIED="1384534798774" TEXT="sujeito (antes de infinitivo)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1384534157169" ID="ID_96787842" MODIFIED="1384534798774" TEXT="final de frase (com palavra de exclus&#xe3;o ou inclus&#xe3;o)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1384534178583" HGAP="119" ID="ID_1109394272" MODIFIED="1384535381240" POSITION="left" TEXT="ME" VSHIFT="-28">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1384534182042" ID="ID_1996446906" MODIFIED="1384534970452" TEXT="entre 2 verbos">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
