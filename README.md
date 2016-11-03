45eed957be0587dabdff1741bb368e94ecbe2a7d
Gilberto Caetano de Andrade <gilbertoca@gmail.com>
2016-11-03 16:14:01 -0300

    Refactoring to use jbake-maven-plugin

---
+++ README.md
@@ -0,0 +1,1 @@
+mvn jbake:inline -Djbake.port=8080 -Djbake.listenAddress=0.0.0.0
--- jbake.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-rm -fr output/
-~/bin/jbake-2.4.0/bin/jbake -b src/jbake/ output
-~/bin/jbake-2.4.0/bin/jbake -s output
